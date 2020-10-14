ce27fafbd7b7982f982d6ed6c886db00c2a27a6d
LaurenPace <lauren.pace-2@selu.edu>
2020-10-14 16:39:06 -0500

    added list of guesses and stops duplicate entries

--- main.cr
+++ main.cr
@@ -57,7 +57,7 @@ key = secret_word.split(//)
 display = [] of String
 i = 0
 while i < key.size
-    if key[i] == " " || key[i] == "-"
+    if key[i] == " " || key[i] == "-" #special characters
         display << key[i]
     else
         display << "_"
@@ -68,36 +68,53 @@ end
 
 playing = true
 incorrect = 0
+guesses = "";
+previousGuess = true
+
 while playing
+    
     system "clear"
     puts hangman_art[incorrect]
+    if guesses != ""
+        print "Previous guesses: ", guesses, "\n"
+    end
     puts display
-    puts "Please enter a letter: "
+    if previousGuess
+        puts "Please enter a letter: "
+    else
+        puts "Letter has already been entered, please enter a different letter."
+    end
     guess = gets.not_nil!
     
-    if key.index(guess) == nil
-        incorrect += 1
-        if incorrect == 6
-            playing = false
-            system "clear"
-            puts hangman_art[incorrect]
-            puts display
-            puts "Game Over"
-        end
-    else
-        j = 0
-        while j < key.size
-            if key[j] == guess
-                display[j] = guess
+    if !guesses.includes?(guess)
+        guesses += guess
+        previousGuess = true
+        if key.index(guess) == nil
+            incorrect += 1
+            if incorrect == 6
+                playing = false
+                system "clear"
+                puts hangman_art[incorrect]
+                puts display
+                print "Game Over... The word was ", secret_word, "\n"
             end
-            j += 1
+        else
+            j = 0
+            while j < key.size
+                if key[j] == guess
+                    display[j] = guess
+                end
+                j += 1
+            end
+            if key == display
+                playing = false
+                system "clear"
+                puts hangman_art[incorrect]
+                puts display
+                puts "Winner!"
+            end
         end
-        if key == display
-            playing = false
-            system "clear"
-            puts hangman_art[incorrect]
-            puts display
-            puts "Winner!"
-        end
+    else 
+        previousGuess = false
     end
 end
